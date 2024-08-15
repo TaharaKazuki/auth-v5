3f3c3c5cb5cff68b85ee01d154d57ce4e3b87f7f
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-08-15 15:09:26 +0900

    update

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,14 @@
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import NextAuth from 'next-auth';
+import Github from 'next-auth/providers/github';
+import Google from 'next-auth/providers/google';
 
 import prisma from './lib/prisma';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  theme: {
+    logo: '/logo.png',
+  },
   adapter: PrismaAdapter(prisma),
-  providers: [],
+  providers: [Google, Github],
 });
