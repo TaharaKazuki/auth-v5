ef4cf90fcc85e8f1913102b59f6353222e8d4360
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-08-15 17:58:23 +0900

    update

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,10 +1,28 @@
+import { auth } from '@/auth';
 import { Metadata } from 'next';
+import { redirect } from 'next/navigation';
 
 export const metadata: Metadata = {
   title: 'Admin',
 };
 
-const AdminPage = () => {
+const AdminPage = async () => {
+  const session = await auth();
+  const user = session?.user;
+
+  if (!user) {
+    redirect('/api/auth/signin?callbackUrl=/admin');
+    return null;
+  }
+
+  if (user.role !== 'admin') {
+    return (
+      <main className="mx-auto my-10">
+        <p className="text-center">You are not authorized to view this page</p>
+      </main>
+    );
+  }
+
   return (
     <main className="mx-auto my-10 space-y-3">
       <h1 className="text-center text-xl font-bold">Admin Page</h1>
