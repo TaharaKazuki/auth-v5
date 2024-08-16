7c1d6e9c3e9e5cc1819f957708afee69dd05eff7
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-08-16 10:09:32 +0900

    update

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,6 +1,7 @@
 import { DefaultSession } from 'next-auth';
 
 declare module 'next-auth' {
+  // eslint-disable-next-line unused-imports/no-unused-vars
   interface Session {
     user: User & DefaultSession['user'];
   }
